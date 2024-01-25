5d23fae3ee1c2eb680db51af11a87f96407e8371
darik0d <70072471+darik0d@users.noreply.github.com>
2024-01-25 18:46:22 +0100

    add chess-hall.tasm

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main() {
     map<string, Color> colorMap{{"A", Color(1.0f, 0.0f, 0.0f, .5f)},
                                 {"W", Color(0.259f, 0.373f, 0.82f, .5f)},
                                 {"GW", Color(0.259f, 0.373f, 0.82f, .5f)},
+                                {"black", Color(0.0f, 0.0f, 0.0f, 1.0f)},
+                                {"white", Color(1.0f, 1.0f, 1.0f, 1.0f)},
                                 {"default", Color(1.0f, 1.0f, 1.0f, 1.0f)}};
     Visualisation v(45.0f, 0.1f, 100.0f, colorMap);
     v.run();
